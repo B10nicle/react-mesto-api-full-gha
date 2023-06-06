aea37114f6cf272962c46bb566c22ba1d27bdb5f
Oleg Khilko <khilkoleg@gmail.com>
2023-06-06 15:33:00 +0300

    fix: mongodb url

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const {
 } = require('./controllers/auth');
 
 const {
-  MONGO_URL = 'mongodb://localhost:27017/mestodb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
   PORT = 3000,
 } = process.env;
 
